e4f9933dd5d5c39fac02074843fc3529a622376c
Okechukwu Uneze <unezeo@gmail.com>
2026-05-12 10:24:14 -0400

    done

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+// This module provides a function to create a login tracker for user authentication attempts.
+const createLoginTracker = (userInfo) => {
+  let attemptCount = 0;
 
-
+  return (passwordAttempt) => {
+    attemptCount++;
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      return "Login successful";
+    } else if (passwordAttempt !== userInfo.password || attemptCount > 3) {
+      if (attemptCount <= 3) {
+        return `Attempt ${attemptCount}: Login failed`;
+      } else {
+        return "Account locked due to too many failed login attempts";
+      }
+    }
+  };
+};
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
